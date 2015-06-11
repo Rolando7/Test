ef634b0e224b4c5f1e38304c6a3852ff219d1851
roland <roland@vbhost>
2015-06-11 17:46:29 +0200

    Test

---
+++ HelloServer/src/helloW.java
@@ -0,0 +1,14 @@
+
+public class helloW {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test");
+		System.out.println("Test");
+
+	}
+
+}
